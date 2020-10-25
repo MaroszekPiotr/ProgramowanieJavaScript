b88bb24fd9cdea16904da95d25ee745a95d29f9a
Piotr Maroszek <piotr.maroszek@microsoft.wsei.edu.pl>
2020-10-25 12:36:12 +0100

    lightbox skończony

--- Cw1_LightBox/main.js
+++ Cw1_LightBox/main.js
@@ -1,30 +1,49 @@
-const images = document.querySelectorAll('.gallery img');
-
-for (let idx = 0; idx < images.length; idx++) {
-    const img = images[idx];
-    img.addEventListener('click', showLightbox);
+const lightBox = document.querySelector('.lightbox');
+const lightBoxImg = document.querySelector('.lightbox img');
+const prevBtn = document.querySelector('.lightbox .prev');
+const nextBtn = document.querySelector('.lightbox .next');
+const imageObjects = [];
+let numberOfImages = undefined;
+for (let i = 0, imgs = [...(document.querySelectorAll('.gallery img'))]; i<imgs.length;i++){
+    imageObjects[i] = {
+        number: i,
+        imageURL: imgs[i].src,     
+    };
+    imgs[i].addEventListener('click', ()=>showLightBox(imageObjects[i]));
+    if (i===(imgs.length - 1)) {
+        numberOfImages = imgs.length - 1;
+    }       
 }
 
-//images.addEventListener('click', showLightBox);
+let prevImg = imageObjects[0];
+let nextImg = imageObjects[0];
 
-function showLightbox(ev) {
-    console.dir(ev.target);
-    const lightbox = document.querySelector('.lightbox');
-    const img = document.querySelector('.lightbox img');
-    const imgUrl = ev.target.src;
-    img.src = imgUrl;
-    lightbox.classList.toggle('visible');
-    console.log('showlight');
-}
+const isExistNearImage = (actualImageNumber, operation) => {
+    let anotherImageNumber = false;
+    switch (operation){
+    case 'next': 
+        if (actualImageNumber.number<numberOfImages) {
+            return imageObjects[(actualImageNumber.number + 1)];
+        }
+        break;
+    case 'prev': 
+        if (actualImageNumber.number>0) {
+            return imageObjects[(actualImageNumber.number - 1)];
+        }
+        break;
+    }
+    return anotherImageNumber;
+};
 
-const openImage = document.querySelector('.lightbox img ');
+const showLightBox = (imageObject) => {
+    lightBoxImg.src = imageObject.imageURL;
+    lightBox.classList.add('visible');    
+    isExistNearImage(imageObject, 'prev') ? prevBtn.classList.remove('displayNone') : prevBtn.classList.add('displayNone');
+    isExistNearImage(imageObject, 'next') ? nextBtn.classList.remove('displayNone') : nextBtn.classList.add('displayNone');
+    prevImg = isExistNearImage(imageObject, 'prev');
+    nextImg = isExistNearImage(imageObject, 'next');
+};
 
-openImage.addEventListener('click', hideLightBox);
-
-function hideLightBox(ev) {
-    console.dir(ev.target);
-    const lightbox = document.querySelector('.lightbox');
-    lightbox.classList.toggle('visible');
-    lightbox.classList.toggle('livebox');
-    console.log('hidelight');
-}
+document.querySelector('.lightbox img').addEventListener('click', ()=>lightBox.classList.remove('visible'));
+prevBtn.addEventListener('click', () => showLightBox(prevImg));
+nextBtn.addEventListener('click', () => showLightBox(nextImg));
