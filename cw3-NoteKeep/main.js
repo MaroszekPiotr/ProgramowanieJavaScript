0d4c6f5df0a52218614907ab4c19b10c27fb3579
Piotr Maroszek <piotr.maroszek@microsoft.wsei.edu.pl>
2020-10-29 20:19:21 +0100

    dodano style i koncepcję wyglądu notatki

---
+++ cw3-NoteKeep/main.js
@@ -0,0 +1,13 @@
+const notes = [];
+
+
+
+const note = {
+    title: 'title',
+    message: 'content',
+    color: 'color',
+    pinned: 0,
+    startDate: new (Date),
+    tag: [],
+    reminder: null,
+};
