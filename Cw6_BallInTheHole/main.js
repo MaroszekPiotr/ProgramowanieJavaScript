91848d0ae3c0904dba39d698a4e3250a1f797a50
Piotr Maroszek <piotr.maroszek@microsoft.wsei.edu.pl>
2020-12-29 12:34:02 +0100

    dzmieniono sterowanie

--- Cw6_BallInTheHole/main.js
+++ Cw6_BallInTheHole/main.js
@@ -18,7 +18,12 @@ class BallInTheHole {
         this.fps = fps;
         this.drawBoard = this.DrawBoard();
         this.playerBall = new GameBall(this); //this.CreatePlayerBall();
-        setInterval(() => this.AnimeGame(), Math.floor(1000 / this.fps));
+        this.targetPoint = new TargetPoint(this);
+        this.gameStatus;
+        this.timeStart;
+        this.timeEnd;
+        this.StartGame();
+
     }
     DrawBoard() {
         this.ctx.beginPath();
@@ -29,6 +34,28 @@ class BallInTheHole {
     AnimeGame() {
         this.DrawBoard();
         this.playerBall.MoveBall();
+        this.targetPoint.Draw();
+    }
+    StartGame() {
+        if (this.gameStatus == null) {
+            this.timeStart = Date.now();
+            this.gameStatus = setInterval(() => this.AnimeGame(), Math.floor(1000 / this.fps));
+        }
+
+
+    }
+    StopGame() {
+        this.timeEnd = Date.now();
+        clearInterval(this.gameStatus);
+        this.gameStatus = null;
+        //console.log(((this.timeEnd - this.timeStart) / 1000).toFixed(2) + 's');
+
+    }
+    Win() {
+
+    }
+    Lose() {
+
     }
 }
 
@@ -59,24 +86,44 @@ class GameBall {
         this.DrawBall();
     }
     ControlBall() {
-        let alpha; //grawitacja lewo prawo 0 pozycja wyjściowa, zakres od -90 (skrajne lewo) do +90 skrajne prawo
-        let beta; //ustawia grawitację dół góra 90 pozycja wyjściowa 0-90 dół, 90-180 góra
-        let gamma;
-        addEventListener('deviceorientation', (e) => {
-            if (e.alpha > -90 && e.alpha < 0) this.direction[0] = (Math.abs(e.alpha / 30) * -1);
-            else if (e.alpha > 0 && e.alpha < 90) this.direction[0] = (Math.abs(e.alpha / 30));
-            if (e.beta > 0 && e.beta < 90) this.direction[1] = (Math.abs(e.beta / 30));
-            else if (e.beta > 90 && e.beta < 180) this.direction[1] = (Math.abs(e.beta / 30) * -1);
-            // else if (e.alpha < 0) this.direction[1] = (e.alpha / 30) * -1;
-            // this.direction[1] = e.alpha / 30;
-            // this.direction[0] = e.beta / 30;
+        window.addEventListener('deviceorientation', (e) => {
+            if (Math.abs(e.gamma) > 90) this.direction[1] = 5;
+            else this.direction[1] = -5;
+            if (Math.abs(e.alpha) > 90) this.direction[0] = 5;
+            else this.direction[0] = -5;
         });
     }
 }
 
+class TargetPoint {
+    constructor(gameClassObj) {
+        this.gameClassObj = gameClassObj;
+        this.ctx = gameClassObj.ctx;
+        this.positionX = Math.floor(Math.random() * (gameClassObj.cw - 40)) + 20;
+        this.positionY = Math.floor(Math.random() * (gameClassObj.ch - 40)) + 20;
+        this.radius = 15;
+        this.color = 'red';
+    }
+    Draw() {
+        this.ctx.beginPath();
+        this.ctx.fillStyle = this.color;
+        this.ctx.arc(this.positionX, this.positionY, this.radius, 0, 2 * Math.PI);
+        this.ctx.fill();
+        this.ctx.closePath();
+    }
 
-// praca z Canvas:
+}
+
+
+// uruchomienie gry:
 const canvas = document.querySelector('canvas');
-
 const game = new BallInTheHole(canvas);
-// addEventListener('deviceorientation', (e) => console.log(e.alpha));
+const gameBar = document.querySelector('.gameBar');
+gameBar.children[0].addEventListener('click', (e) => {
+    e.preventDefault();
+    game.StartGame();
+});
+gameBar.children[1].addEventListener('click', (e) => {
+    e.preventDefault();
+    game.StopGame();
+});
